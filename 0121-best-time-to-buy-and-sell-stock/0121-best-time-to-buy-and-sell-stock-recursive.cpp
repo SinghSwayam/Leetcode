178d701a3e74492d136b73ffed21bc0a60ef3e89
Swayam <swayamsingh.dev@gmail.com>
2026-03-19 10:13:10 +0530

    Time: 0 ms (100%), Space: 97.2 MB (86.42%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock-recursive.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void maxProfitFinder(vector<int>& prices,int i,int& minPrice,int& maxProfit){
+
+        if(i >= prices.size()) return;
+
+        if(prices[i] < minPrice) 
+            minPrice = prices[i];
+
+        int todaysPrice=prices[i] - minPrice;
+
+        if(todaysPrice > maxProfit)
+            maxProfit = todaysPrice;
+
+        maxProfitFinder(prices,i+1,minPrice,maxProfit);
+
+    }
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX;
+        int maxProfit = INT_MIN;
+        
+        maxProfitFinder(prices,0,minPrice,maxProfit);
+
+        return maxProfit;
+    }
+};
