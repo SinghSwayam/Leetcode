b6cfc5dcd5230c3fde137ad3b738b83c1a62fa9f
Swayam <swayamsingh.dev@gmail.com>
2026-04-10 21:09:08 +0530

    Time: 50 ms (37.16%), Space: 7.9 MB (82.1%) - LeetHub

---
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(vector<int>& arr,int& n,int& ans,int currNum){
+        if(currNum == n+1){
+            ans++;
+            return;
+        }
+
+        for(int i=1;i<=n;++i){
+            if(arr[i] == 0 && (currNum % i == 0 || i % currNum == 0) ){
+                arr[i] = currNum;
+                solve(arr,n,ans,currNum+1);
+                arr[i] = 0;
+            }
+        }
+    }
+
+    int countArrangement(int n) {
+        vector<int> arr(n+1);
+        int ans=0;
+
+        solve(arr,n,ans,1);
+
+        return ans;
+    }
+};
