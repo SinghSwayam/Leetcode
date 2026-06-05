e416adc00b2a00fec21977fa4cdcc336cba8d3f8
Swayam <swayamsingh.dev@gmail.com>
2026-06-05 17:22:43 +0530

    Time: 234 ms (11.01%), Space: 13.3 MB (18.64%) - LeetHub

--- 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
+++ 3753-total-waviness-of-numbers-in-range-ii/3753-total-waviness-of-numbers-in-range-ii.cpp
@@ -1,50 +1,64 @@
 class Solution {
 public:
     vector<int> d;
+    // DP State: [pos][waviness_sum][prev2_digit][prev_digit][is_strictly_smaller][is_non_zero_started]
     long long dp[30][30][11][11][2][2];
 
-    long long solve(int pos,int sum,int prev2,int prev,bool small,bool nz){
+    long long solve(int pos, int sum, int prev2, int prev, bool small, bool nz) {
         if(pos == d.size()) return sum;
+        
         if(dp[pos][sum][prev2][prev][small][nz] != -1) {
             return dp[pos][sum][prev2][prev][small][nz];
         }
 
+        // limit is 9 if we are already smaller than the prefix, else it's the actual digit
         int lim = small ? 9 : d[pos];
         long long ans = 0;
-        for(int i=0;i<=lim;i++){
-            bool nsmall = (small || i<d[pos]);
-            bool nnz = (nz || i!=0);
+        
+        for(int i = 0; i <= lim; i++) {
+            bool nsmall = (small || i < d[pos]); // Becomes true if we pick a smaller digit
+            bool nnz = (nz || i != 0);           // Becomes true once we place the first non-zero digit
 
-            if(nnz){
+            if(nnz) {
+                // leading zeros ended
                 int nprev2 = prev;
                 int nprev = i;
                 int nsum = sum;
-                if(prev2!=10 && prev!=10){
+                
+                // If we have at least 3 digits to compare, check for a wave
+                if(prev2 != 10 && prev != 10) {
+                    // peak: prev2 < prev > current
                     nsum += (prev2 < prev && prev > i);
+                    // valley: prev2 > prev < current
                     nsum += (prev2 > prev && prev < i);
                 }
-                ans += solve(pos+1,nsum,nprev2,nprev,nsmall,nnz);
-            }else{
-                ans += solve(pos+1,sum,prev2,prev,nsmall,nnz);
+                ans += solve(pos + 1, nsum, nprev2, nprev, nsmall, nnz);
+            } else {
+                // processing leading zeros
+                ans += solve(pos + 1, sum, prev2, prev, nsmall, nnz);
             }
         }
+        
+        // Memoize and return
         dp[pos][sum][prev2][prev][small][nz] = ans;
         return ans;
     }
 
-    long long count(long long num){
+    long long count(long long num) {
         if(num == 0) return 0;
         d.clear();
-        while(num){
-            d.push_back(num%10);
+        while(num) {
+            d.push_back(num % 10);
             num /= 10;
         }
-        reverse(d.begin(),d.end());
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,10,10,0,0);
+        reverse(d.begin(), d.end());
+        memset(dp, -1, sizeof(dp));
+        // start dp by pos=0, sum=0, prev2=10, prev=10, small=0, nz=0
+        return solve(0, 0, 10, 10, 0, 0);
     }
 
     long long totalWaviness(long long num1, long long num2) {
-        return count(num2) - count(num1-1);
+        // digit dp range technique : ans(R) - ans(L-1)
+        return count(num2) - count(num1 - 1);
     }
 };
