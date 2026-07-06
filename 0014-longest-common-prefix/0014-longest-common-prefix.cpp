8f5a62d3607bf7c8052573c8864f391fae0c1fc7
Swayam <swayamsingh.dev@gmail.com>
2026-07-06 11:20:13 +0530

    Time: 2 ms (10.98%), Space: 12.4 MB (6.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,24 +1,68 @@
+class TrieNode { 
+public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+    int childCount = 0;
+
+    TrieNode(char d){
+        this->data = d;
+        for(int i=0; i<26; i++){
+            children[i] = NULL;
+        }
+        childCount = 0;
+        this->isTerminal = false;
+    }
+};
+
 class Solution {
+    void insertWord(TrieNode* root, const string &word, int pos){
+        if(pos == word.size()){
+            root->isTerminal = true;
+            return;
+        }
+
+        int index = word[pos] - 'a';
+
+        if(root->children[index] == NULL){
+            root->children[index] = new TrieNode(word[pos]);
+            root->childCount++;
+        }
+
+        insertWord(root->children[index], word, pos + 1);
+    }
+
+    void findLCP(TrieNode* root, string first, string& ans){
+        if(root->isTerminal) return;
+
+        for(int i=0; i< first.length(); i++){
+            char ch = first[i];
+
+            if(root->childCount == 1){
+                ans.push_back(ch);
+                int index = ch - 'a';
+                root = root->children[index];
+            }else{
+                break;
+            }
+
+            if(root->isTerminal)
+                break;
+        }
+    }
+
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string ans = "";
-
-        for(int i=0; i<strs[0].size(); i++){
-            char ch = strs[0][i];
-            bool match = true;
-
-            for(int j=1; j<strs.size(); j++){
-                if(strs[j].size() < i || ch != strs[j][i]){
-                    match = false;
-                    break;
-                }
-            }
-            if(!match){
-                break;
-            }else{
-                ans.push_back(ch);
-            }
+        TrieNode* root = new TrieNode('-');
+        for(string str : strs){
+            insertWord(root,str,0);
         }
+
+        string ans = "";
+        string first = strs[0];
+
+        findLCP(root, first, ans);
+        
         return ans;
     }
 };
