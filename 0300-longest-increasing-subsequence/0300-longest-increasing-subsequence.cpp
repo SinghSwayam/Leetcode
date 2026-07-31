24146ac815f942a7c80f8d18e907e40072f7e1fb
Swayam <swayamsingh.dev@gmail.com>
2026-07-31 11:07:41 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.49%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -70,6 +70,21 @@ public:
         return next[0];
     }
 
+    int solveBinarySearch(vector<int>& nums){
+        vector<int> ans;
+        ans.push_back(nums[0]);
+
+        for(int i=1; i<nums.size(); i++){
+            if(nums[i] > ans.back()){
+                ans.push_back(nums[i]);
+            }else{
+                int index = lower_bound(ans.begin(), ans.end(), nums[i]) - ans.begin();
+                ans[index] = nums[i];
+            }
+        }
+        return ans.size();
+    }
+
     int lengthOfLIS(vector<int>& nums) {
         int curr = 0;
         int prev = -1;
@@ -81,7 +96,9 @@ public:
 
         // int ans = solveTabulation(nums);
 
-        int ans = solveTabulationSpaceOptimized(nums);
+        // int ans = solveTabulationSpaceOptimized(nums);
+
+        int ans = solveBinarySearch(nums);
         return ans;
     }
 };
