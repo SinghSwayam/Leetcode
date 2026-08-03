3c06df1e69e416ef5c09f44e144fb2e64145ebe4
Swayam <swayamsingh.dev@gmail.com>
2026-08-03 10:58:14 +0530

    Time: 85 ms (19.37%), Space: 11 MB (83.25%) - LeetHub

---
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int solveRecursion(int s, int e){
+        if(s >= e){
+            return 0;
+        }
+        int mini = INT_MAX;
+        for(int i=s; i<=e; i++){
+            int curr = i + max(solveRecursion(s, i-1), solveRecursion(i+1, e));
+            mini = min(mini, curr);
+        }
+        return mini;
+    }
+
+    int solveMemoization(int s, int e, vector<vector<int>>& dp){
+        if(s >= e){
+            return 0;
+        }
+
+        if(dp[s][e] != -1) return dp[s][e];
+
+        int mini = INT_MAX;
+        for(int i=s; i<=e; i++){
+            int curr = i + max(solveMemoization(s, i-1, dp), solveMemoization(i+1, e, dp));
+            mini = min(mini, curr);
+        }
+        return dp[s][e] = mini;
+    }
+
+    int getMoneyAmount(int n) {
+        // int ans = solveRecursion(0, n);
+
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1)); 
+        int ans = solveMemoization(0, n, dp);
+        return ans;
+    }
+};
