1ed71708c66cb035f14171a80e0fd9bd84e7fb0c
Swayam <swayamsingh.dev@gmail.com>
2026-08-06 19:09:58 +0530

    Time: 35 ms (6.38%), Space: 15 MB (8.7%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int solveRec(vector<int>& arr, map<pair<int,int>, int>& maxi,int s, int e){
+        if(s >= e) return 0;
+
+        int minSum = INT_MAX;
+        for(int i=s; i<e; i++){
+            int curr = (maxi[{s, i}] * maxi[{i+1, e}]) + 
+                        solveRec(arr, maxi, s, i) + 
+                        solveRec(arr, maxi, i+1, e);
+            
+            minSum = min(minSum, curr);
+        }
+        return minSum;
+    }
+
+    int solveMemo(vector<int>& arr, map<pair<int,int>, int>& maxi,int s, int e, vector<vector<int>>& dp){
+        if(s >= e) return 0;
+
+        if(dp[s][e] != -1) return dp[s][e];
+
+        int minSum = INT_MAX;
+        for(int i=s; i<e; i++){
+            int curr = (maxi[{s, i}] * maxi[{i+1, e}]) + 
+                        solveMemo(arr, maxi, s, i, dp) + 
+                        solveMemo(arr, maxi, i+1, e, dp);
+            
+            minSum = min(minSum, curr);
+        }
+        return dp[s][e] = minSum;
+    }
+
+    int mctFromLeafValues(vector<int>& arr) {
+        int n = arr.size();
+        map<pair<int,int>, int> maxi;
+        for(int i=0; i<n; i++){
+            maxi[{i,i}] = arr[i];
+
+            for(int j=i+1; j<n; j++){
+                maxi[{i,j}] = max(arr[j], maxi[{i,j-1}]);
+            }
+        }
+
+        int s = 0;
+        int e = n-1;
+        // int ans = solveRec(arr, maxi, s, e);
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        int ans = solveMemo(arr, maxi, s, e, dp);
+
+        return ans;
+    }
+};
