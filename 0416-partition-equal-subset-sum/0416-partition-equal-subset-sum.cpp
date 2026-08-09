125825e2577df2acb9875cddc08b5102ec9e0047
Swayam <swayamsingh.dev@gmail.com>
2026-08-09 17:56:02 +0530

    Time: 77 ms (83.37%), Space: 15.7 MB (74.42%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,77 @@
 class Solution {
 public:
-    bool solveRec(vector<int>& nums, int target, int i){
+    bool solveRec(vector<int>& nums, int i, int target){
         if(target == 0) return true;
         if(i >= nums.size() || target < 0) return false;
 
         bool inc = false;
         if(target - nums[i] >= 0){
-            inc = solveRec(nums, target - nums[i], i+1);
+            inc = solveRec(nums, i+1, target - nums[i]);
         }
-        bool exc = solveRec(nums, target, i+1);
+        bool exc = solveRec(nums, i+1, target);
 
         return (inc || exc);
     }
 
-    bool solveMemo(vector<int>& nums, int target, int i, vector<vector<int>>& dp){
+    bool solveMemo(vector<int>& nums, int i, int target, vector<vector<int>>& dp){
         if(target == 0) return true;
         if(i >= nums.size() || target < 0) return false;
 
-        if(dp[target][i] != -1) return dp[target][i];
+        if(dp[i][target] != -1) return dp[i][target];
 
         bool inc = false;
         if(target - nums[i] >= 0){
-            inc = solveMemo(nums, target - nums[i], i+1, dp);
+            inc = solveMemo(nums, i+1, target - nums[i], dp);
         }
-        bool exc = solveMemo(nums, target, i+1, dp);
+        bool exc = solveMemo(nums, i+1, target, dp);
 
-        return dp[target][i] = (inc || exc);
+        return dp[i][target] = (inc || exc);
+    }
+
+    bool solveTab(vector<int>& nums, int target){
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+        for(int row=0; row<=n; row++){
+            dp[row][0] = 1;
+        }
+
+        for(int index = n-1; index >= 0; index--){
+            for(int t=1; t<=target; t++){
+                bool inc = false;
+                if(t - nums[index] >= 0){
+                    inc = dp[index+1][t - nums[index]];
+                }
+                bool exc = dp[index+1][t];
+
+                dp[index][t] = (inc || exc);
+            }
+        }
+        return dp[0][target];
+    }
+
+    bool solveTabSpaceOptimized(vector<int>& nums, int target){
+        int n = nums.size();
+        
+        vector<int> curr(target+1, 0);
+        vector<int> next(target+1, 0);
+
+        curr[0] = 1;
+        next[0] = 1;
+
+        for(int index = n-1; index >= 0; index--){
+            for(int t=1; t<=target; t++){
+                bool inc = false;
+                if(t - nums[index] >= 0){
+                    inc = next[t - nums[index]];
+                }
+                bool exc = next[t];
+
+                curr[t] = (inc || exc);
+            }
+            next = curr;
+        }
+        return next[target];
     }
 
     bool canPartition(vector<int>& nums) {
@@ -34,12 +80,16 @@ public:
             total += num;
         }
 
-        if((total % 2) == 1) return false;
+        if(total & 1) return false;
 
         int target = total/2;
-        // return solveRec(nums, target, 0);
+        // return solveRec(nums, 0, target);
 
-        vector<vector<int>> dp(target+1, vector<int>(nums.size()+1, -1));
-        return solveMemo(nums, target, 0, dp);
+        // vector<vector<int>> dp(nums.size()+1, vector<int>(target+1, -1));
+        // return solveMemo(nums, 0, target, dp);
+
+        // return solveTab(nums, target);
+
+        return solveTabSpaceOptimized(nums, target);
     }
 };
