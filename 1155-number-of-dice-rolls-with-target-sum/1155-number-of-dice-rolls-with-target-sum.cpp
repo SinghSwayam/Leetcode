e061ea72a8797badac9e980127c6090f2d67c635
Swayam <swayamsingh.dev@gmail.com>
2026-08-12 17:54:33 +0530

    Time: 17 ms (34.52%), Space: 11.3 MB (54.96%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -34,10 +34,32 @@ public:
         return dp[n][target] = ans;
     }
 
+    int solveTab(int n, int k, int target){
+        vector<vector<int>> dp(n+1, vector<int>(target+1, 0));
+
+        dp[0][0] = 1;
+
+        for(int n_idx = 1; n_idx<=n; n_idx++){
+            for(int t_idx = 1; t_idx <= target; t_idx++){
+
+                int ans = 0;
+                for(int i=1; i<=k && i <= t_idx ;i++){
+                    int temp = dp[n_idx-1][t_idx-i]% mod;
+                    ans = (ans + temp) % mod;
+                }
+                dp[n_idx][t_idx] = ans;
+
+            }
+        }
+        return dp[n][target];
+    }
+
     int numRollsToTarget(int n, int k, int target) {
         // return solveRec(n, k, target);
 
-        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
-        return solveMemo(n, k, target, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        // return solveMemo(n, k, target, dp);
+
+        return solveTab(n, k, target);
     }
 };
