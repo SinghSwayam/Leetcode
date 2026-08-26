839e86ede0f4553f759abb7eb5e14c1a0453ccca
Swayam <swayamsingh.dev@gmail.com>
2026-08-26 21:56:49 +0530

    Time: 0 ms (100%), Space: 8.7 MB (64.74%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+        int l=0, ones=0;
+        string ans = "";
+
+        for(int r=0; r<n; r++){
+            if(s[r] == '1'){
+                ones++;
+            }
+
+            while(ones > k){
+                if(s[l] = '1'){
+                    ones--;
+                }
+                l++;
+            }
+
+            if(ones == k){
+                while(l <= r && s[l] == '0'){
+                    l++;
+                }
+                string curr = s.substr(l, r-l+1);
+
+                if(ans.empty() || (curr.size() < ans.size()) || (curr.size() == ans.size() && curr < ans)){
+                    ans = curr;
+                }
+            }
+        }
+        return ans;
+    }
+};
