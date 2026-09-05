01ac3756f1537d436f28870837e5c8f9fee99809
Swayam <swayamsingh.dev@gmail.com>
2026-09-05 13:00:50 +0530

    Time: 4 ms (5.46%), Space: 18.7 MB (8.05%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int delrow[4] = {-1, 0, 1, 0};
+    int delcol[4] = {0, 1, 0, -1};
+
+    void bfs(vector<vector<int>>& image, int sr, int sc, int color, vector<vector<int>>& visited, int n, int m){
+        int originalColor = image[sr][sc];
+        visited[sr][sc] = 1;
+        image[sr][sc] = color;
+        queue<pair<int, int>> q;
+        q.push({sr, sc});
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++){
+                int nrow = r + delrow[i];
+                int ncol = c + delcol[i];
+
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && !visited[nrow][ncol] && image[nrow][ncol] == originalColor){
+                    visited[nrow][ncol] = 1;
+                    image[nrow][ncol] = color;
+                    q.push({nrow, ncol});
+                }
+            }
+        }            
+    }
+
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n = image.size();
+        int m = image[0].size();
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        bfs(image, sr, sc, color, visited, n, m);
+        return image;
+    }
+};
